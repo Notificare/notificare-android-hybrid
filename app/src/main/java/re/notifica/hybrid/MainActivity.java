fe5e8fd545fb472b753b7fcf5de82fe5e3a89f63
Joel Oliveira <joeloliveira1979@gmail.com>
2017-01-03 10:57:23 +0100

    added config file, added url schemes and preferences file and manifest entries

--- app/src/main/java/re/notifica/hybrid/MainActivity.java
+++ app/src/main/java/re/notifica/hybrid/MainActivity.java
@@ -1,26 +1,106 @@
 package re.notifica.hybrid;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+import re.notifica.Notificare;
+import re.notifica.beacon.BeaconRangingListener;
+import re.notifica.model.NotificareApplicationInfo;
+import re.notifica.model.NotificareBeacon;
+
+public class MainActivity extends AppCompatActivity implements Notificare.OnNotificareReadyListener, BeaconRangingListener {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    protected static final String TAG = MainActivity.class.getSimpleName();
+    private AlertDialog.Builder builder;
+    protected Config config;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        config = new Config(this);
+
         WebView webView =  (WebView) findViewById(R.id.webView);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        webView.loadUrl("https://code.pt");
+        webView.loadUrl(config.getProperty("url"));
         webView.setWebViewClient(new WebViewClient() {
             public void onPageFinished(WebView view, String url) {
                 view.loadUrl("javascript:window.scrollTo(0.0, 100.0);");
             }
         });
+
+        Notificare.shared().addNotificareReadyListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Notificare.shared().removeNotificareReadyListener(this);
+    }
+
+    @Override
+    public void onNotificareReady(NotificareApplicationInfo notificareApplicationInfo) {
+        if (!Notificare.shared().hasLocationPermissionGranted()) {
+            Log.i(TAG, "permission not granted");
+            if (Notificare.shared().didRequestLocationPermission()) {
+                if (Notificare.shared().shouldShowRequestPermissionRationale(this)) {
+                    // Here we should show a dialog explaining location updates
+                    builder.setMessage(R.string.alert_location_permission_rationale)
+                            .setTitle(R.string.app_name)
+                            .setCancelable(true)
+                            .setPositiveButton(R.string.button_location_permission_rationale_ok, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    Notificare.shared().requestLocationPermission(MainActivity.this, LOCATION_PERMISSION_REQUEST_CODE);
+                                }
+                            })
+                            .create()
+                            .show();
+                }
+            } else {
+                Notificare.shared().requestLocationPermission(this, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE:
+                if (Notificare.shared().checkRequestLocationPermissionResult(permissions, grantResults)) {
+                    Log.i(TAG, "permission granted");
+                    Notificare.shared().enableLocationUpdates();
+                    if (BuildConfig.ENABLE_BEACONS) {
+                        Notificare.shared().enableBeacons(60000);
+                    }
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onRangingBeacons(List<NotificareBeacon> list) {
+
     }
 }
